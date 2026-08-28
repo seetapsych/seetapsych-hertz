040709cdc41a335bd79525d6ba1f0f63ea93b410
levalup <likier@sina.cn>
2026-08-28 17:41:36 +0800

    add example

--- tests/heart_rate.py
+++ tests/heart_rate.py
@@ -9,32 +9,27 @@ from seetapsych_lib.runtime.factory import Factory
 from seetapsych_lib.runtime.pipeline import Pipeline
 from seetapsych_lib.runtime.parallel_runner import ParallelRunner as Runner
 
+module_roots = [
+    os.path.join(os.path.dirname(__file__), '../seetapsych_hertz/modules'),
+    os.path.join(os.path.dirname(__file__), '../../seetapsych-face-hub/seetapsych_face_hub/modules'),
+    os.path.join(os.path.dirname(__file__), '../../seetapsych-face-ex/seetapsych_face_ex/modules'),
+]
+
 
 def main():
     factory = Factory()
-    factory.load_builtin_modules()
-
-    factory.load_dir_modules(os.path.join(os.path.dirname(__file__), '../seetapsych_hertz/modules'))
-    factory.load_dir_modules(os.path.join(os.path.dirname(__file__), '../../seetapsych-face-hub/seetapsych_face_hub/modules'))
-    factory.load_dir_modules(os.path.join(os.path.dirname(__file__), '../../seetapsych-face-ex/seetapsych_face_ex/modules'))
 
     pipeline = Pipeline(factory, packages=[
-        'c938b879-44db-45b0-9a5d-8377f0ace5e5', # insightface's face/detection
-        'bb212f54-aace-438f-9cb7-f6519f4fef48', # face/selection
+        # '08e29e70-e21b-4ef2-8486-5aaec5f8295b', # SeetaHeartRateDetector
+        # '23871da8-0968-4034-ac48-35641ae67d63', # TinyHR
+        '3d98f435-d484-4b91-acf4-f690c28b409f', # AdaChrom
     ], attributes=[
-        'face/dense_landmarks',
-        'face/heart_rate'
+        # 'face/heart_rate',
     ])
-    pipeline.set_models(
-        'c938b879-44db-45b0-9a5d-8377f0ace5e5',
-        ['1deb39b1-1074-4f6e-b81e-a6a843d011eb']    # using minimum retinaface model
-    )
-
-    # print(pipeline.config.model_dump_json(indent=2, exclude_none=True))
 
     pipeline.solve()
-
-    # print(pipeline.config.model_dump_json(indent=2, exclude_none=True))
+    pipeline.install_requirements()
+    pipeline.cache_models()
 
     print(pipeline.problem())
     print(pipeline.satisfied())
@@ -45,6 +40,8 @@ def main():
     if not cap.isOpened():
         print('Could not open camera')
         exit(1)
+
+    hr: float | None = None
 
     while True:
         ok = cap.grab()
@@ -59,18 +56,11 @@ def main():
         frame_height, frame_width = frame.shape[:2]
         face_detection = report.get('face_detection', [])
         face_selection = report.get('face_selection', {'pid': 0})
-        face_landmarks = report.get('face_dense_landmarks', [])
-        for bbox, landmarks in zip(face_detection, face_landmarks):
+        for bbox in face_detection:
             xyxy = bbox['xyxy']
             # score = bbox['score']
             xyxy = list(map(int, xyxy))
-
-            point2ds = numpy.asarray(landmarks['landmarks']).reshape([-1, 2])
-
             cv2.rectangle(frame, xyxy[:2], xyxy[2:], (255, 0, 0), 2)
-            for p in point2ds:
-                p = list(map(int, p))
-                cv2.circle(frame, p, 2, (0, 255, 0), -1)
 
         # print(face_detection)
         heart_rate = report.get('face_heart_rate', {})
@@ -85,7 +75,7 @@ def main():
 
             cv2.putText(frame, f'{pid}', p, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             wait_seconds = heart_rate.get('wait_seconds', 0)
-            hr = heart_rate.get('hr', None)
+            hr = heart_rate.get('hr_bpm', None) or hr
             cv2.putText(frame, f'wait: {wait_seconds:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             if hr is not None:
                 cv2.putText(frame, f'hr: {hr:.0f}bpm', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
